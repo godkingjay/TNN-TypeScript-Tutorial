8b82bdbb2e1caeb60b2be6245f05d5a30de7f5d0
Jarrian <glorianagojar@gmail.com>
2022-12-26 09:55:46 +0800

    Lesson 2 [DONE]

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
-var character = 'mario';
+var character = 'luigi';
 console.log(character);
 var inputs = document.querySelectorAll('input');
+console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,10 @@
-const character = 'mario';
+const character = 'luigi';
 
 console.log(character);
 
 const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
 
 inputs.forEach(input => {
   console.log(input);
