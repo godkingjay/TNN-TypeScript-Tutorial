6b7059f019dafefd9f9c9fd00574e3280fb689d1
Jarrian <glorianagojar@gmail.com>
2022-12-27 08:37:04 +0800

    Lesson 4 [DONE]

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,26 @@
-// arrays
-var names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
+//  arrays
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
 // names.push(3);
-// names[1] = 3;
-var numbers = [10, 20, 12, 15];
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
+// names[0] = 3;
+var numbers = [10, 20, 30, 40, 50, 60];
+// numbers.push(`jay`);
+// numbers[2] = `jay`;
+var mixed = ["jay", 1, "v", 8, 10];
+mixed.push("ryu");
+mixed.push(3);
 mixed[0] = 3;
-// objects
+//  object
 var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
+    name: "jay",
+    belt: "jin",
+    age: 21
 };
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
+ninja.age = 12;
+ninja.name = "v";
+// ninja.age = `ten`;
 ninja = {
-    name: 'yoshi',
-    belt: 'orange',
+    name: "yoshi",
+    belt: "orange",
     age: 40
 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,35 @@
-// arrays
-let names = ['luigi', 'mario', 'yoshi'];
+//  arrays
 
-names.push('toad');
+let names = [`luigi`, `mario`, `yoshi`];
+
+names.push(`toad`);
 // names.push(3);
-// names[1] = 3;
+// names[0] = 3;
 
-let numbers = [10, 20, 12, 15];
+let numbers = [10, 20, 30, 40, 50, 60];
+// numbers.push(`jay`);
+// numbers[2] = `jay`;
 
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
+let mixed = [`jay`, 1, `v`, 8, 10];
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-
-mixed.push('ryu');
-mixed.push(10);
+mixed.push(`ryu`);
+mixed.push(3);
 mixed[0] = 3;
 
-// objects
+//  object
+
 let ninja = {
-  name: 'mario',
-  belt: 'black',
-  age: 30
+  name: `jay`,
+  belt: `jin`,
+  age: 21
 };
 
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
+ninja.age = 12;
+ninja.name = `v`;
+// ninja.age = `ten`;
 
 ninja = {
-  name: 'yoshi',
-  belt: 'orange',
+  name: `yoshi`,
+  belt: `orange`,
   age: 40,
-  // skills: ['running'],
 };
