b1db2dc0546c0d196ca81915b45456b79326bfb0
Jarrian <glorianagojar@gmail.com>
2022-12-27 08:41:16 +0800

    ADD: LESSON 5

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,3 @@
-var character = 'mario';
-var age;
-var isLoggedIn;
-// age = 'luigi';
-age = 30;
-// isLoggedIn = 25;
-isLoggedIn = true;
 // arrays
-var ninjas = [];
-ninjas.push('ryu');
-ninjas.push('chun-li');
-console.log(ninjas);
 // union types
-var mixed = [];
-mixed.push('hello');
-mixed.push(false);
-mixed.push(20);
-console.log(mixed);
-var uid;
 // objects
-var ninjaOne;
-ninjaOne = { name: 'yoshi', age: 30 };
-var ninjaTwo;
-ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
--- sandbox.ts
+++ sandbox.ts
@@ -1,36 +1,9 @@
-let character: string = 'mario';
-let age: number;
-let isLoggedIn: boolean;
 
-// age = 'luigi';
-age = 30;
-
-// isLoggedIn = 25;
-isLoggedIn = true;
 
 // arrays
-let ninjas: string[] = [];
 
-ninjas.push('ryu');
-ninjas.push('chun-li');
-console.log(ninjas);
 
 // union types
-let mixed: (string|number|boolean)[] = [];
-mixed.push('hello');
-mixed.push(false);
-mixed.push(20);
-console.log(mixed);
 
-let uid: string|number;
 
 // objects
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
-
-let ninjaTwo: {
-  name: string,
-  age: number,
-  beltColour: string
-};
-ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
