085bc1f36ada9774057d55aebc6fa7d05e25538f
Jarrian <glorianagojar@gmail.com>
2023-01-02 14:38:45 +0800

    Lesson 5 [DONE]

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,35 @@
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'mario';
+age = 13;
+// isLoggedIn = 12;
+isLoggedIn = true;
 // arrays
+var ninjas = [];
+// ninjas = ['luigi', 'mario'];
+ninjas.push('luigi');
 // union types
+var mixed = [];
+mixed.push('mario');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
 // objects
+var ninja1;
+// ninja1 = 123;
+// ninja1 = [];
+ninja1 = {
+    name: 'luigi',
+    age: 30
+};
+var ninja2;
+ninja2 = {
+    name: 'mario',
+    age: 20,
+    beltColor: 'black'
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,51 @@
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
+// age = 'mario';
+age = 13;
+
+// isLoggedIn = 12;
+isLoggedIn = true;
 
 // arrays
+let ninjas: string[] = [];
 
+// ninjas = ['luigi', 'mario'];
+ninjas.push('luigi');
 
 // union types
+let mixed: (string | number | boolean)[] = [];
 
+mixed.push('mario');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+
+let uid: string | number;
+
+uid = '123';
+uid = 123;
 
 // objects
+let ninja1: object;
+// ninja1 = 123;
+// ninja1 = [];
+ninja1 = {
+  name: 'luigi',
+  age: 30,
+};
+
+let ninja2: {
+  name: string,
+  age: number,
+  beltColor: string,
+};
+
+ninja2 = {
+  name: 'mario',
+  age: 20,
+  beltColor: 'black',
+  // skills: [],
+};
