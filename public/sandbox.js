ee7ee1054af874c182a8e9bfec7aeb935d495c98
Jarrian <glorianagojar@gmail.com>
2023-01-03 13:09:38 +0800

    Lesson 9 [DONE]

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,19 +1,13 @@
 "use strict";
-// let greet: Function = () => {
-//   console.log('hello, world');
-// }
-// greet = 'hello';
-// greet = () => {
-//   console.log('hello, again');
-// }
-var add = function (a, b, c /*?*/) {
-    if (c === void 0) { c /*?*/ = 10; }
-    console.log(a + b);
-    console.log(c);
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}.`);
 };
-add(5, 10, 'ninja');
-var minus = function (a, b) {
-    return a + b;
+const greet = (user) => {
+    console.log(`${user.name}, says hello!`);
 };
-var result = minus(10, 7);
-console.log(result);
+// const greetAgain = (user: {
+//   name: string,
+//   uid: string | number
+// }) => {
+//   console.log(`${user.name}, says hello!`);
+// }
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,20 @@
-// let greet: Function = () => {
-//   console.log('hello, world');
-// }
+type StringOrNum = string | number;
+type ObjWithName = {
+  name: string,
+  uid: StringOrNum,
+};
 
-// greet = 'hello';
-
-// greet = () => {
-//   console.log('hello, again');
-// }
-
-const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
-  console.log(a + b);
-  console.log(c);
+const logDetails = (uid: StringOrNum | number, item: string) => {
+  console.log(`${item} has a uid of ${uid}.`);
 }
 
-add(5, 10, 'ninja');
-
-const minus = (a: number, b: number): number => {
-  return a + b;
+const greet = (user: ObjWithName) => {
+  console.log(`${user.name}, says hello!`);
 }
 
-let result = minus(10,7);
-console.log(result);
+// const greetAgain = (user: {
+//   name: string,
+//   uid: string | number
+// }) => {
+//   console.log(`${user.name}, says hello!`);
+// }
