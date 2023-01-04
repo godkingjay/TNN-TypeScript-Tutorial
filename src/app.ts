bc61332bc98c808ae4065f1086543f28d2a4c26d
Jarrian <glorianagojar@gmail.com>
2023-01-04 15:04:40 +0800

    Lesson 12 [DONE]

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#type') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
