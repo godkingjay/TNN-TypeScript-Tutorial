302bddf6360756af0e51768bd4f126107b196d5f
Jarrian <glorianagojar@gmail.com>
2023-01-04 15:05:16 +0800

    Lesson 13 [DONE]

--- public/app.js
+++ public/app.js
@@ -25,7 +25,7 @@ const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#type');
+const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
