91dcc05b7d44199956578629f3df5212a1ec9705
Jarrian <glorianagojar@gmail.com>
2023-01-04 15:22:05 +0800

    Lesson 17 [DONE]

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
-import { Invoice } from './classes/Invoice.js';
-import { Payment } from './classes/Payment.js';
-import { ListTemplate } from './classes/ListTemplate.js';
-import { HasFormatter } from './interfaces/HasFormatter.js';
+import { Invoice } from "./classes/Invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
 
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
-// list template instance
-const ul = document.querySelector('ul')!;
+//  list template instance
+
+const ul = document.querySelector('.item-list') as HTMLUListElement;
 const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
-
+  
   let doc: HasFormatter;
-  if (type.value === 'invoice') {
+  
+  if(type.value.match(/INVOICE/gi)) {
     doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  
+
   list.render(doc, type.value, 'end');
 });
